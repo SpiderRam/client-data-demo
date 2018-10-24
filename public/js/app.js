bd708c954e2cb2e85d4c30086dc4f9d0a3401fbb
Carmen Owen <doctorsout@yahoo.com>
2018-10-24 17:11:09 -0400

    Basic pagination functionality working, json data being returned and rendered

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+new Vue({
+	el:'#vue',
+	data:{
+        clients: [],
+        limit: 5,
+        skip: 0
+  },
+  methods: {
+    getClients: function() {
+        var self = this;
+        $.ajax({
+            type: "GET",
+            url: "/clients"
+        }).then(function(response) {
+            // response = JSON.parse(response);
+            console.log(response);
+            self.clients = response;
+        });
+    }
+  },
+  created: function() {
+      var self = this;
+      self.getClients();
+  },
+  computed: {
+  	page(){
+    	return this.clients.slice(this.skip, this.skip + this.limit);
+    }
+  }
+});
---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const clients = require("../data/generated");
+
+module.exports = function(app) {
+
+    app.get('/clients', function(req,res) {
+        res.json(clients);
+        // console.log(clients);
+    });
+
+};
